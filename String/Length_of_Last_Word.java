866af09719ddad954f0549866ae4c222a5d3e745
Anery <137743057@qq.com>
2018-07-16 17:44:05 +0800

    Create Length_of_Last_Word.java

---
+++ String/Length_of_Last_Word.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        char[] c = s.toCharArray();
+        int len=0;
+        for(int i = c.length-1 ; i >= 0 ; i--){
+            if(c[i] != ' ')
+                len++;
+            else if(len > 0)
+                    break;
+        }
+        return len;
+    }
+}
